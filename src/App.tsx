d53a5e44f9d4edff6ee66b61e404104f06937d4f
luongminhhieu.mta <luongminhhieu.mta@gmail.com>
2022-08-01 20:01:16 +0700

    add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
+import { Route, Routes } from 'react-router-dom';
 import './styles/App.css';
+import Home from './components/home';
+import Settings from './components/settings';
+import Editor from './components/editor';
+import User from './components/user';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+      <Routes>
+        <Route path={ '/' } element={ <Home/> }/>
+        <Route path={ '/settings' } element={ <Settings/> }/>
+        <Route path={ '/editor' } element={ <Editor/> }/>
+        <Route path={ '/user' } element={ <User/> }/>
+      </Routes>
     </div>
   );
 }
